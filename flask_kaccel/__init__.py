02f1d878119da2dcf10da811a61c7717b1edcf29
adierebel <adie.rebel@gmail.com>
2020-09-01 16:14:43 +0700

    drop python 2.x support, fix minor bugs

--- flask_kaccel/__init__.py
+++ flask_kaccel/__init__.py
@@ -1,127 +1,120 @@
 __import__('pkg_resources').declare_namespace(__name__)
 from flask import Response, request
 from .mime import MIME
-import os
-
-try:
-	from flask import _app_ctx_stack as stack
-except ImportError:
-	from flask import _request_ctx_stack as stack
+from .version import version
+from os import path
 
 class Kaccel(object):
 	'''Class used to control the Kaccel integration to a Flask application.
 
-    You can use this by providing the Flask app on instantiation or by calling
-    an :meth:`init_app` method an instance object of `Kaccel`. Here an
-    example of providing the application on instantiation: ::
+	You can use this by providing the Flask app on instantiation or by calling
+	an :meth:`init_app` method an instance object of `Kaccel`. Here an
+	example of providing the application on instantiation: ::
 
-        app = Flask(__name__)
-        kaccel = Kaccel(app)
+		app = Flask(__name__)
+		kaccel = Kaccel(app)
 
-    And here calling the :meth:`init_app` method: ::
+	And here calling the :meth:`init_app` method: ::
 
-        kaccel = Kaccel()
+		kaccel = Kaccel()
 
-        def init_app():
-            app = Flask(__name__)
-            kaccel.init_app(app)
-            return app
+		def init_app():
+			app = Flask(__name__)
+			kaccel.init_app(app)
+			return app
 
-    :Config:
+	:Config:
 
 	- KACCEL_HOST
-	- KACCEL_BASE_PATH
+	- KACCEL_REDIRECT_PATH
+	- KACCEL_FILES_PATH
 	- KACCEL_BUFFER
 	- KACCEL_CHARSET
 	- KACCEL_CACHE_EXPIRES
 	- KACCEL_LIMIT_RATE
 
-    '''
+	'''
 	def __init__(self, app=None):
+		# Set
 		self.app = app
-		if app is not None:
+		self.version = version
+
+		# Init app
+		if app:
 			self.init_app(app)
 
 	def init_app(self, app):
-		config_host		= app.config.get('KACCEL_HOST')
-		config_path 	= app.config.get('KACCEL_BASE_PATH')
-		config_buffer 	= app.config.get('KACCEL_BUFFER')
-		config_charset 	= app.config.get('KACCEL_CHARSET')
-		config_expires 	= app.config.get('KACCEL_CACHE_EXPIRES')
-		config_limit 	= app.config.get('KACCEL_LIMIT_RATE')
+		# Get config
+		config_host = app.config.get('KACCEL_HOST')
+		config_redirect_path = app.config.get('KACCEL_REDIRECT_PATH')
+		config_files_path = app.config.get('KACCEL_FILES_PATH')
+		config_buffer = app.config.get('KACCEL_BUFFER')
+		config_charset = app.config.get('KACCEL_CHARSET')
+		config_expires = app.config.get('KACCEL_CACHE_EXPIRES')
+		config_limit = app.config.get('KACCEL_LIMIT_RATE')
 	
-		if config_host:
-			self.host = config_host
-		else:
-			self.host = request.host
+		# Set config
+		self.host = config_host or request.host
+		self.files_path = config_files_path or None
+		self.redirect_path = config_redirect_path or None
+		self.buffering = config_buffer or True
+		self.charset = config_charset or "utf-8"
+		self.cache_expires = config_expires or False
+		self.limit_rate = config_limit or False
 
-		if config_path:
-			self.redirect_path = config_path
+	def _bool(self, value):
+		if value:
+			return "yes"
 		else:
-			self.redirect_path = "/files/%s"
+			return "no"
+	
+	def _off(self, value):
+		if not value:
+			return "off"
+		return value
 
-		if config_buffer:
-			self.buffering = 'yes'
-		else:
-			self.buffering = 'no'
-
-		if config_charset:
-			self.charset = config_charset
-		else:
-			self.charset = "utf-8"
-
-		if config_expires:
-			self.cache_expires = config_expires
-		else:
-			self.cache_expires = 'off'
-
-		if config_limit:
-			self.limit_rate = config_limit
-		else:
-			self.limit_rate = 'off'
-
-	def filesize(self, file):
+	def filesize(self, file_path):
 		''' get file size in byte
 		
-		:param file: fullpath
-		:return: return <integer> if success and False if failed.
+		:param file_path: fullpath
+		:return: return <integer> if success and None if fail.
 		'''
 		try:
-			return os.path.getsize(file)
+			return path.getsize(file_path)
 		except:
-			return False
+			return None
 
-	def filename(self, file):
+	def filename(self, file_path):
 		''' get file name from full path.
 		
-		:param file: fullpath
-		:return: return <string> if success and False if failed.
+		:param file_path: fullpath
+		:return: return <string> if success and None if fail.
 		'''
 		try:
-			return os.path.basename(file)
+			return path.basename(file_path)
 		except:
-			return False
+			return None
 
-	def extension(self, file):
+	def extension(self, file_path):
 		''' get extension from full path.
 
-		:param file: fullpath
-		:return: return <string> if success and False if failed.
+		:param file_path: fullpath
+		:return: return <string> if success and None if fail.
 		'''
 		try:
-			name, ext = os.path.splitext(self.filename(file))
+			name, ext = path.splitext(self.filename(file_path))
 			return ext
 		except:
-			return False
+			return None
 
-	def mimetype(self, file):
+	def mimetype(self, file_path):
 		''' get mime type from file
 
-		:param file: fullpath
+		:param file_path: fullpath
 		:return: return <string> if success and return "application/octet-stream" if mime type not found.
 		'''
 		try:
-			ext  = self.extension(file)
+			ext = self.extension(file_path)
 			mime = MIME.get(ext)
 			if not mime:
 				mime = "application/octet-stream"
@@ -129,46 +122,62 @@ class Kaccel(object):
 		except:
 			return "application/octet-stream"
 
-	def send_file(self, file, redirect="/files/%s", buffering='yes', charset='utf-8', expires='off', limit='off'):
+	
+	def send_from_directory(self, filename):
+		''' send file from directory using global configuration.
+
+		:param filename: file name
+		:return: return Request() object if success and None if fail.
+		'''
+		return self.send_file(
+			file_path = path.join(self.files_path, filename),
+			redirect = self.redirect_path,
+			buffering = self.buffering,
+			charset = self.charset,
+			expires = self.cache_expires,
+			limit = self.limit_rate
+		)
+
+	def send_file(
+			self,
+			file_path,
+			redirect = None,
+			buffering = True,
+			charset = 'utf-8',
+			expires = None,
+			limit = None
+		):
 		''' send file from directory using custom configuration.
 
-		:param file: fullpath.
-		:param redirect: redirect path, default= "/files/%s"
-		:param buffering: sets the proxy buffering for this connection, value= "yes" | "no"
-		:param charset: sets the charset of the file, default= "utf-8"
-		:param expires: sets when to expire the file in the internal NGINX cache, value= "off" | seconds
-		:param limit: sets the rate limit for this single request. off means unlimited, value= "off" | bytes
-		:return: return Request() object if success and False if failed.
+		:param file_path: fullpath.
+		:param redirect: redirect path, default=None
+		:param buffering: sets the proxy buffering for this connection, value=(True|False), default=True
+		:param charset: sets the charset of the file, default="utf-8"
+		:param expires: sets when to expire the file in the internal NGINX cache, value=(None|int), default=None
+		:param limit: sets the rate limit for this single request. off means unlimited, value=(None|int), default=None
+		:return: return Request() object if success and None if fail.
 		'''
 		try:
 			if self.host == request.host:
-				return "Error: direct access is forbidden"
+				return None
 
-			content_length	= self.filesize(file)
-			content_type 	= self.mimetype(file)
-			filename		= self.filename(file)
+			content_length	= self.filesize(file_path)
+			content_type 	= self.mimetype(file_path)
+			filename		= self.filename(file_path)
 
 			if not content_length or not content_type or not filename:
-				return False
+				return None
 
-			resp = Response()
-			resp.headers['Content-Length'] 		= content_length
-			resp.headers['Content-Type'] 		= content_type
-			resp.headers['Content-Disposition'] = "attachment; filename=%s" % (str(filename))
-			resp.headers['X-Accel-Redirect'] 	= redirect % (str(filename))
-			resp.headers['X-Accel-Buffering'] 	= buffering
-			resp.headers['X-Accel-Charset'] 	= charset
-			resp.headers['X-Accel-Expires'] 	= expires
-			resp.headers['X-Accel-Limit-Rate'] 	= limit
-			return resp
-		
+			response = Response()
+			response.headers['Content-Length'] = content_length
+			response.headers['Content-Type'] = content_type
+			response.headers['Content-Disposition'] = "attachment; filename=%s" % (str(filename))
+			response.headers['X-Accel-Redirect'] = path.join(redirect, filename)
+			response.headers['X-Accel-Buffering'] = self._bool(buffering)
+			response.headers['X-Accel-Charset'] = charset
+			response.headers['X-Accel-Expires'] = self._off(expires)
+			response.headers['X-Accel-Limit-Rate'] = self._off(limit)
+			return response
+
 		except:
-			return False
-
-	def send_from_directory(self, file):
-		''' send file from directory using global configuration.
-
-		:param file: fullpath
-		:return: return Request() object if success and False if failed.
-		'''
-		return self.send_file(file=file, redirect=self.redirect_path, buffering=self.buffering, charset=self.charset, expires=self.cache_expires, limit=self.limit_rate)
+			return None
--- setup.py
+++ setup.py
@@ -1,28 +1,40 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from flask_kaccel.version import version
+
+with open('README.md') as f:
+	long_description = f.read()
 
 setup(
-    name='Flask-Kaccel',
-    version='1.0',
-    url='https://flask-kaccel.readthedocs.io/en/latest/',
-    license='MIT',
-    author='LovaPOS',
-    author_email='dev@lovapos.com',
-    description='Add Flask support for Nginx X-Accel',
-    packages=['flask_kaccel'],
-    namespace_packages=['flask_kaccel'],
-    zip_safe=False,
-    platforms='any',
-    install_requires=[
-        'Flask'
-    ],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
+	name='Flask-Kaccel',
+	version=version,
+	url='https://flask-kaccel.readthedocs.io/en/latest/',
+	license='MIT',
+	author='LovaPOS',
+	author_email='dev@lovapos.com',
+	description='Add Flask support for Nginx X-Accel',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	packages=find_packages(include=['flask_kaccel']),
+	namespace_packages=['flask_kaccel'],
+	zip_safe=False,
+	platforms='any',
+	install_requires=[
+		'Flask'
+	],
+	project_urls={
+		'Documentation': 'https://flask-kaccel.readthedocs.io/en/latest/',
+		'Source': 'https://github.com/lovapos/Flask-Kaccel',
+	},
+	keywords='flask nginx x-accel',
+	python_requires='>=3',
+	classifiers=[
+		'Development Status :: 4 - Beta',
+		'Environment :: Web Environment',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 3',
+		'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+	]
 )
